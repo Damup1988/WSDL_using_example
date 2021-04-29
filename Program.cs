4afe18b3656c311f7d3305287a9f69f0866871b3
Damir Safarov <damup1988@gmail.com>
2021-04-29 03:15:55 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSDL_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! It's a wasdl using example");
+            Console.ReadLine();
+        }
+    }
+}
