56bc6372c46e1ec73dbf0f7433f4295756deb2fb
Damir Safarov <damup1988@gmail.com>
2021-04-29 03:24:41 +0300

    wsdl has been used

--- Program.cs
+++ Program.cs
@@ -12,6 +12,10 @@ namespace WSDL_Example
         {
             Console.WriteLine("Hello! It's a wasdl using example");
             Console.ReadLine();
+
+            Calculator.CalculatorSoapClient client = new Calculator.CalculatorSoapClient();            
+            Console.WriteLine($"sum of 4 and 5 is: {client.Add(4, 5)}");
+            Console.ReadLine();
         }
     }
 }
